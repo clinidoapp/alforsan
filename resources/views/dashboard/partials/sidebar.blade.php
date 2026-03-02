8623e9c9acc0e1cdd2d057fd51aed82204979b2d
MayadaRefaat <mayadarefaat7595@gmail.com>
2026-03-02 14:22:04 +0200

    update icon

--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -136,7 +136,7 @@
     @if(\App\Helpers\Permissions::hasPermission('read_about_us'))
       <li class="nav-item rounded-2 m-2">
          <a href="{{route('about-us')}}" class="nav-link {{ Request::is(patterns: 'admin/about-us*') ? 'active' : '' }}">
-         <img src="{{ asset('images/dashboard-icons/setting.webp') }}">
+         <img src="{{ asset('images/dashboard-icons/about-us.webp') }}">
          About Us
          </a>
       </li>
